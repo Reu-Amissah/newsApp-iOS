6c47df6b05be8cde1b07095bd8912f908cda8870
Reu-Amissah <reujoyamissah@gmail.com>
2022-10-20 23:37:01 +0000

    initial commit - create xcode project

---
+++ itsNew/itsNew/itsNewApp.swift
@@ -0,0 +1,17 @@
+//
+//  itsNewApp.swift
+//  itsNew
+//
+//  Created by R. Amissah on 20/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct itsNewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
