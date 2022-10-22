32009dcdebfbadcfca2e36420458c4eb1f1a9b2c
Reu-Amissah <reujoyamissah@gmail.com>
2022-10-22 08:24:45 +0000

    add button color, background color and logo

--- itsNew/itsNew/Views/ContentView.swift
+++ itsNew/itsNew/Views/ContentView.swift
@@ -9,35 +9,40 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Spacer()
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            HStack {
-                Text("Welcome to ")
-                Text("itsNew")
-                    .foregroundColor(.orange)
-            }
-            .font(.title2)
-            .bold()
-            .padding(.top)
-            
-            Text("Stay up to date with latest news around the world")
-                .multilineTextAlignment(.center)
-                .padding(.top)
-            
-            Spacer()
-            Text("Get Started")
-                .padding()
-                .frame(maxWidth: .infinity)
-                .background(.blue)
-                .cornerRadius(10)
-                .foregroundColor(.white)
+        ZStack {
+            Color("backgroundColor")
+                .edgesIgnoringSafeArea(.all)
+            VStack (spacing: 10) {
+                Spacer()
+                Image("logo")
+                    .imageScale(.large)
+                    .foregroundColor(.accentColor)
+                    .frame(width: 92, height: 92)
+                    .cornerRadius(50)
+                HStack {
+                    Text("Welcome to ")
+                        .foregroundColor(Color("textColorLanding"))
+                    Text("itsNew")
+                        .foregroundColor(.orange)
+                }
+                .font(.title2)
                 .bold()
-            
+                
+                Text("Stay up to date with latest news around the world").foregroundColor(Color("textColorLanding"))
+                    .multilineTextAlignment(.center)
+                
+                Spacer()
+                Text("Get Started")
+                    .padding()
+                    .frame(maxWidth: .infinity)
+                    .background(Color("buttonColor"))
+                    .cornerRadius(30)
+                    .foregroundColor(.white)
+                    .bold()
+                
+            }
+            .padding()
         }
-        .padding()
     }
 }
 
