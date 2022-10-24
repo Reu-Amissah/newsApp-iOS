d2924ef7c957de8f69a2655d68ce6ed7ab6dd6c8
Reu-Amissah <reujoyamissah@gmail.com>
2022-10-24 20:24:35 +0000

    add login screen

--- itsNew/itsNew/Views/LoginView.swift
+++ itsNew/itsNew/Views/LoginView.swift
@@ -12,25 +12,74 @@ struct LoginView: View {
         VStack {
             Spacer()
             LoginHeaderView()
-                .multilineTextAlignment(.leading)
+                .padding(.horizontal, 20)
+            InputSection()
             Spacer()
             LoginButtonView()
+                .padding(.horizontal, 20)
+            Spacer()
         }
     }
 }
 
+
+//Input Fields Model------
+struct InputSection: View {
+    var body: some View {
+        InputFieldView(username: "", text: "Email")
+            .padding(.horizontal, 20)
+            .padding(.top, 30)
+        InputFieldView(username: "", text: "Password")
+            .padding(.horizontal, 20)
+            .padding(.top, 10)
+        
+    }
+}
+
+//Login Header Text -- Welcome Text
 struct LoginHeaderView: View {
     var body: some View {
-        LoginBoldText(text: "Welcome Back 👋")
-            .multilineTextAlignment(.trailing)
-        LoginSecondaryText(text: "This would take less than a minute, enter your username, email and password")
+        VStack(spacing: 15) {
+            LoginBoldText(text: "Welcome Back 👋")
+                .frame(maxWidth: .infinity, alignment: .leading)
+            LoginSecondaryText(text: "This would take less than a minute, enter your username, email and password")
+                .frame(maxWidth: .infinity, alignment: .leading)
+        }
+    }
+}
+
+struct InputFieldView: View {
+    @State var username: String
+    var text: String
+    
+    var body: some View {
+        TextField(text, text: $username)
+            .padding()
+            .foregroundColor(Color("secondaryTextColor"))
+            .background(
+                RoundedRectangle(cornerRadius: 10)
+                    .fill(
+                        Color("inputFieldBackgroundColor")
+                         )
+            )
     }
 }
 
 struct LoginButtonView: View {
     var body: some View {
-        Text("Not a member? Sign Up")
-        Text("Login")
+        
+        VStack (spacing: 20) {
+            Text("Login")
+                .padding()
+                .frame(maxWidth: .infinity)
+                .background(Color("buttonColor"))
+                .cornerRadius(10)
+                .foregroundColor(.white)
+                .bold()
+                .font(.title3)
+            Text("Not a member? Sign Up")
+                .font(.callout)
+        }
     }
 }
 
